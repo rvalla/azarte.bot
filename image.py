7fb4187f4f3bc430323299205cd9a07dfb124a3d
Rodrigo Valla <rodrigovalla@protonmail.ch>
2021-11-12 19:15:11 -0300

    Improving the Image class...

--- image.py
+++ image.py
@@ -3,7 +3,7 @@ from io import BytesIO
 import math
 import random as rd
 import time as tm
-from PIL import Image as im, ImageDraw as idraw, ImageFilter as ifil
+from PIL import Image as im, ImageDraw as idraw
 
 class Image():
 	"The class the bot uses to process the images..."
@@ -13,33 +13,35 @@ class Image():
 		self.w, self.h = self.get_image_size()
 		self.c = (self.w/2, self.h/2)
 		self.lw = 3
-		self.scale = rd.randint(5,12)
-		self.angles = self.get_angles(rd.randint(3,16))
+		self.scale = rd.randint(8,12)
+		self.angles = self.get_angles(rd.randint(3,12))
 		self.sizes = self.get_escape_sizes(len(self.angles))
 		self.mask_path = "assets/img/masks/"
 		self.input_mask_list = [f for f in os.listdir(self.mask_path) if not f.startswith(".")]
 
+	#Update method to change random variables configuration
 	def update(self):
 		self.w, self.h = self.get_image_size()
-		self.scale = rd.randint(5,20)
-		self.angles = self.get_angles(rd.randint(3,16))
+		self.scale = rd.randint(8,12)
+		self.angles = self.get_angles(rd.randint(3,12))
 		self.sizes = self.get_escape_sizes(len(self.angles))
 
+	#Drawing an escape art piece...
 	def draw_escape(self):
 		i = im.new("RGB",(self.w, self.h),self.background)
 		margin = self.w / 10
 		d = idraw.Draw(i)
-		m = rd.choice([0,1])
 		c = self.get_time_color()
-		for r in range(200):
-			p = (self.w/2,self.h/3*2)
+		for r in range(144):
+			p = self.get_escape_start_point()
 			c = self.move_color(c,3)
 			while self.is_point_in(p, margin):
-				np = self.get_dice_point(m, p)
+				np = self.get_dice_point(p)
 				self.draw_line(d,c,self.lw,p,np)
 				p = np
 		return self.create_image(i)
 
+	#Drawing an artistic clock...
 	def draw_clock(self):
 		i = im.new("RGB",(self.w, self.h),self.background)
 		cs = (self.w/10, self.h/10)
@@ -61,6 +63,7 @@ class Image():
 		self.draw_circle(d, self.move_color((35,70,180),5), p3, cs[0])
 		return self.create_image(i)
 
+	#Drawing lines joining two random walks...
 	def draw_lines(self):
 		i = im.new("RGB",(self.w, self.h),self.background)
 		d = idraw.Draw(i)
@@ -68,7 +71,7 @@ class Image():
 		p1 = (self.w/2,self.h/5)
 		p2 = (self.w/2,self.h/5*4)
 		margin = self.w / 10
-		for r in range(2500):
+		for r in range(2584):
 			np1 = self.get_random_motion(p1)
 			np2 = self.get_random_motion(p2)
 			if self.is_point_in(np1, margin) and self.is_point_in(np2, margin):
@@ -78,6 +81,7 @@ class Image():
 				c = self.move_color(c,1)
 		return self.create_image(i)
 
+	#Drawing a beautiful rando histogram...
 	def draw_distribution(self):
 		i = im.new("RGB",(self.w, self.h),self.background)
 		d = idraw.Draw(i)
@@ -97,36 +101,44 @@ class Image():
 			b = (b[0], b[1] + m)
 			self.draw_rectangle(d, color, a, b)
 			color = self.move_color(color, 5)
-		return self.create_image(i)
+		return i#self.create_image(i)
 
+	#Creating a random data list to draw a distribution...
 	def get_distribution(self, steps):
 		data = [1 for i in range(steps)]
-		type = rd.choice([0,1])
+		type = rd.choice([0,1,2])
 		for v in range(100):
 			if type == 0:
 				data[rd.randint(0,len(data)-1)] += 1
-			else:
+			elif type == 1:
 				r = (rd.randint(0,len(data)-1) + rd.randint(0,len(data)-1)) // 2
+				data[r] += 1
+			else:
+				a = rd.randint(0,len(data)-1)
+				b = rd.randint(0,len(data)-1)
+				r = 0
+				if a < b:
+					r = a
+				else:
+					r = b
 				data[r] += 1
 		return data
 
+	#Function to move a point randomly...
 	def get_random_motion(self, last_point):
 		x = rd.randint(-30,30)
 		y = rd.randint(-30,30)
 		return (last_point[0] + x, last_point[1] + y)
 
-	def get_dice_point(self, mode, last_point):
+	#Function to move a point for a escape drawing...
+	def get_dice_point(self, last_point):
 		c = len(self.angles)
-		if mode == 0:
-			r = rd.randint(0,c-1)
-			x = math.cos(self.angles[r]) * self.sizes[r] * self.scale
-			y = math.sin(self.angles[r]) * self.sizes[r] * self.scale
-		else:
-			r = round((rd.randint(0,c-1) + rd.randint(0,c-1))/2)
-			x = math.cos(self.angles[r]) * self.sizes[r] * self.scale
-			y = math.sin(self.angles[r]) * self.sizes[r] * self.scale
+		r = rd.randint(0,c-1)
+		x = math.cos(self.angles[r]) * self.sizes[r] * self.scale
+		y = math.sin(self.angles[r]) * self.sizes[r] * self.scale
 		return (last_point[0] + x, last_point[1] + y)
 
+	#Checking if a point is in the canvas...
 	def is_point_in(self, point, margin):
 		is_in = True
 		if (point[0] < margin or point[0] > self.w - margin
@@ -134,17 +146,21 @@ class Image():
 			is_in = False
 		return is_in
 
+	#Function to draw a line...
 	def draw_line(self, draw, c, w, a, b):
 		draw.line([a,b], fill=c, width=w)
 
+	#Function to draw a circle...
 	def draw_circle(self, draw, color, c, d):
 		a = (c[0] - d/2, c[1] - d/2)
 		b = (c[0] + d/2, c[1] + d/2)
 		draw.ellipse([a,b], fill=color, outline=None)
 
+	#Function to draw a rectangle...
 	def draw_rectangle(self, draw, color, a, b):
 		draw.rectangle([a,b], fill=color, outline=None)
 
+	#Getting a random color based on time...
 	def get_time_color(self):
 		t = tm.localtime()
 		r = t.tm_sec * 2
@@ -152,19 +168,23 @@ class Image():
 		b = t.tm_hour * 5
 		return (r,g,b)
 
+	#Moving a color randomly in rgb color space...
 	def move_color(self, color, motion):
 		r = rd.randint(-motion,motion)
 		g = rd.randint(-motion,motion)
 		b = rd.randint(-motion,motion)
 		return ((color[0]+r)%255,(color[1]+g)%255,(color[2]+b)%255)
 
+	#A function to invert a color...
 	def invert_color(self, color):
 		return (255-color[0], 255-color[1], 255-color[2])
 
+	#A function to chose a default image size to work with...
 	def get_image_size(self):
 		size = rd.choice([(1080,1920),(1080,1080),(1920,1080),(1920,1440),(1440,1920)])
 		return size[0], size[1]
 
+	#A function to obtain a list of angles in radian for the escape drawing algorithm...
 	def get_angles(self, count):
 		ang = []
 		v = 2 * math.pi / count
@@ -172,8 +192,9 @@ class Image():
 			ang.append(a * v)
 		return ang
 
+	#A function to obtain a list of relative sizes for the escape drawing algorithm...
 	def get_escape_sizes(self, count):
-		aux = [rd.randint(1,6) for r in range(count)]
+		aux = [rd.randint(1,4) for r in range(count)]
 		aux.sort()
 		offset = rd.randint(0,count)
 		sizes = []
@@ -181,6 +202,13 @@ class Image():
 			sizes.append(aux[(i+offset)%count])
 		return sizes
 
+	#Adding some noise to the start point...
+	def get_escape_start_point(self):
+		x = self.w/5 + rd.random() * self.w/5 * 3
+		y = self.h/5 + rd.random() * self.h/5 * 3
+		return (x,y)
+
+	#A function to get an image combining two images...
 	def mean_image(self, back_image, top_image):
 		back_image.convert("RGBA")
 		top_image.convert("RGBA").resize(back_image.size)
@@ -188,6 +216,7 @@ class Image():
 		new = im.composite(back_image, top_image, mask)
 		return self.create_image(i)
 
+	#A function to paint a mask with an input image...
 	def mask_merge(self, background, top_image):
 		top_image.convert("RGBA")
 		mask = im.open(self.mask_path + rd.choice(self.input_mask_list)).convert("L").resize(top_image.size)
@@ -195,6 +224,7 @@ class Image():
 		new = im.composite(back_image, top_image, mask)
 		return self.create_image(i)
 
+	#The function to store the Image object in a byte stream...
 	def create_image(self, image):
 		file = BytesIO()
 		image.save(file, "jpeg", quality=85, optimize=True)
